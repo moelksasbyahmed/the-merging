c905272b89190afb1f6db1001b202d1f34c75c2f
ETG <123107746+EveryTGames@users.noreply.github.com>
2024-05-01 21:06:19 +0300

    added the coloring system

--- Assets/ahmed/scripts/ColorManager.cs
+++ Assets/ahmed/scripts/ColorManager.cs
@@ -1,25 +1,37 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ColorManager : MonoBehaviour
 {
-    [SerializeField] List<Vector2> positions;
-    
+    [SerializeField] List<Color> CollectedColors;
+    public Light2D light;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-       
 
 
-        
+
+
+    }
+
+
+    public    void newColor(Color color)
+    {
+        if (CollectedColors.Count <= 6)
+        {
+
+            CollectedColors.Add(color);
+            light.color = Color.Lerp(light.color, color, 0.5f);
+        }
+
     }
 
 }
---
+++ Assets/ahmed/scripts/ColorOrbs.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorOrbs : MonoBehaviour
+{
+    public ColorManager manager;
+    public Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player")) 
+        {
+            manager.newColor(color);
+            Destroy(gameObject);
+            
+        }
+    }
+}
